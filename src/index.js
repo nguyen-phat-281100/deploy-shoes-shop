e4328a738fad7bfaf85719d6e8209915d5370fc1
nguyen-phat-281100 <85777996+nguyen-phat-281100@users.noreply.github.com>
2021-10-30 23:51:52 +0700

    shoeshop

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import "jquery/dist/jquery";
+import "bootstrap/dist/js/bootstrap";
+import "popper.js/dist/umd/popper";
+import "bootstrap/dist/css/bootstrap.min.css";
+import App from "./App";
+import store from "./store";
+import { Provider } from "react-redux";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      {/* store đầu là props ,cái thứ 2 là giá trị */}
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
